497dc5304627b06eb0198615889379ed19613b44
chichunchen <chichunchen844@gmail.com>
2015-07-15 01:05:05 +0800

    Add schedule in nearby.

--- app/src/main/java/com/example/idreams/dot/NearbyScheduleBar.java
+++ app/src/main/java/com/example/idreams/dot/NearbyScheduleBar.java
@@ -6,12 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ShowNearbyFragment extends Fragment {
-
+public class NearbyScheduleBar extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_show_nearby, container, false);
+        return inflater.inflate(R.layout.fragment_nearby_schedule_bar, container, false);
     }
 }
