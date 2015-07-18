ed73243ed2351937cd4a5e8a76d56e15f9a9d799
chichunchen <chichunchen844@gmail.com>
2015-07-18 18:34:58 +0800

    Connect two LatLng successfully.

--- app/src/main/java/com/example/idreams/dot/navigate/NavigateActivity.java
+++ app/src/main/java/com/example/idreams/dot/navigate/NavigateActivity.java
@@ -1,17 +1,10 @@
 package com.example.idreams.dot.navigate;
 
-import android.app.ProgressDialog;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AutoCompleteTextView;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.directions.route.Route;
@@ -21,10 +14,6 @@ import com.example.idreams.dot.BaseActivity;
 import com.example.idreams.dot.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.PlaceBuffer;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -32,33 +21,18 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
-import butterknife.ButterKnife;
-import butterknife.InjectView;
-import butterknife.OnClick;
-
 public class NavigateActivity extends BaseActivity implements RoutingListener, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
-    private static final LatLngBounds BOUNDS_JAMAICA = new LatLngBounds(new LatLng(-57.965341647205726, 144.9987719580531),
-            new LatLng(72.77492067739843, -9.998857788741589));
+
     protected GoogleMap map;
     protected LatLng start;
     protected LatLng end;
     protected GoogleApiClient mGoogleApiClient;
-    @InjectView(R.id.start)
-    AutoCompleteTextView starting;
-    @InjectView(R.id.destination)
-    AutoCompleteTextView destination;
-    @InjectView(R.id.send)
-    ImageView send;
     private String LOG_TAG = "NavigateActivity";
-    private PlaceAutoCompleteAdapter mAdapter;
-    private ProgressDialog progressDialog;
     private Polyline polyline;
 
     /**
@@ -68,7 +42,6 @@ public class NavigateActivity extends BaseActivity implements RoutingListener, G
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigate);
-        ButterKnife.inject(this);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -86,23 +59,6 @@ public class NavigateActivity extends BaseActivity implements RoutingListener, G
             getSupportFragmentManager().beginTransaction().replace(R.id.map, mapFragment).commit();
         }
         map = mapFragment.getMap();
-
-        mAdapter = new PlaceAutoCompleteAdapter(this, android.R.layout.simple_list_item_1,
-                mGoogleApiClient, BOUNDS_JAMAICA, null);
-
-
-        /*
-        * Updates the bounds being used by the auto complete adapter based on the position of the
-        * map.
-        * */
-        map.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
-            @Override
-            public void onCameraChange(CameraPosition position) {
-                LatLngBounds bounds = map.getProjection().getVisibleRegion().latLngBounds;
-                mAdapter.setBounds(bounds);
-            }
-        });
-
 
         CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(18.013610, -77.498803));
         CameraUpdate zoom = CameraUpdateFactory.zoomTo(16);
@@ -151,7 +107,6 @@ public class NavigateActivity extends BaseActivity implements RoutingListener, G
 
                         map.moveCamera(center);
                         map.animateCamera(zoom);
-
                     }
 
                     @Override
@@ -170,173 +125,25 @@ public class NavigateActivity extends BaseActivity implements RoutingListener, G
                     }
                 });
 
-
-
-        /*
-        * Adds auto complete adapter to both auto complete
-        * text views.
-        * */
-        starting.setAdapter(mAdapter);
-        destination.setAdapter(mAdapter);
-
-
-        /*
-        * Sets the start and destination points based on the values selected
-        * from the autocomplete text views.
-        * */
-
-        starting.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                final PlaceAutoCompleteAdapter.PlaceAutocomplete item = mAdapter.getItem(position);
-                final String placeId = String.valueOf(item.placeId);
-                Log.i(LOG_TAG, "Autocomplete item selected: " + item.description);
-
-            /*
-             Issue a request to the Places Geo Data API to retrieve a Place object with additional
-              details about the place.
-              */
-                PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
-                        .getPlaceById(mGoogleApiClient, placeId);
-                placeResult.setResultCallback(new ResultCallback<PlaceBuffer>() {
-                    @Override
-                    public void onResult(PlaceBuffer places) {
-                        if (!places.getStatus().isSuccess()) {
-                            // Request did not complete successfully
-                            Log.e(LOG_TAG, "Place query did not complete. Error: " + places.getStatus().toString());
-                            places.release();
-                            return;
-                        }
-                        // Get the Place object from the buffer.
-                        final Place place = places.get(0);
-
-                        start = place.getLatLng();
-                    }
-                });
-
-            }
-        });
-        destination.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                final PlaceAutoCompleteAdapter.PlaceAutocomplete item = mAdapter.getItem(position);
-                final String placeId = String.valueOf(item.placeId);
-                Log.i(LOG_TAG, "Autocomplete item selected: " + item.description);
-
-            /*
-             Issue a request to the Places Geo Data API to retrieve a Place object with additional
-              details about the place.
-              */
-                PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
-                        .getPlaceById(mGoogleApiClient, placeId);
-                placeResult.setResultCallback(new ResultCallback<PlaceBuffer>() {
-                    @Override
-                    public void onResult(PlaceBuffer places) {
-                        if (!places.getStatus().isSuccess()) {
-                            // Request did not complete successfully
-                            Log.e(LOG_TAG, "Place query did not complete. Error: " + places.getStatus().toString());
-                            places.release();
-                            return;
-                        }
-                        // Get the Place object from the buffer.
-                        final Place place = places.get(0);
-
-                        end = place.getLatLng();
-                    }
-                });
-
-            }
-        });
-
-        /*
-        These text watchers set the start and end points to null because once there's
-        * a change after a value has been selected from the dropdown
-        * then the value has to reselected from dropdown to get
-        * the correct location.
-        * */
-        starting.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int startNum, int before, int count) {
-                if (start != null) {
-                    start = null;
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-        destination.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-
-                if (end != null) {
-                    end = null;
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
+        sendRequest();
     }
 
-    @OnClick(R.id.send)
-    public void sendRequest() {
+    private void sendRequest() {
         if (Util.Operations.isOnline(this)) {
-            route();
+            start = new LatLng(18.015365, -77.499382);
+            end = new LatLng(18.012590, -77.500659);
+
+            Routing routing = new Routing(Routing.TravelMode.WALKING);
+            routing.registerListener(this);
+            routing.execute(start, end);
         } else {
             Toast.makeText(this, "No internet connectivity", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void route() {
-        if (start == null || end == null) {
-            if (start == null) {
-                if (starting.getText().length() > 0) {
-                    starting.setError("Choose location from dropdown.");
-                } else {
-                    Toast.makeText(this, "Please choose a starting point.", Toast.LENGTH_SHORT).show();
-                }
-            }
-            if (end == null) {
-                if (destination.getText().length() > 0) {
-                    destination.setError("Choose location from dropdown.");
-                } else {
-                    Toast.makeText(this, "Please choose a destination.", Toast.LENGTH_SHORT).show();
-                }
-            }
-        } else {
-            progressDialog = ProgressDialog.show(this, "Please wait.",
-                    "Fetching route information.", true);
-            Routing routing = new Routing(Routing.TravelMode.WALKING);
-            routing.registerListener(this);
-            routing.execute(start, end);
-        }
-    }
-
-
     @Override
     public void onRoutingFailure() {
         // The Routing request failed
-        progressDialog.dismiss();
         Toast.makeText(this, "Something went wrong, Try again", Toast.LENGTH_SHORT).show();
     }
 
@@ -347,12 +154,10 @@ public class NavigateActivity extends BaseActivity implements RoutingListener, G
 
     @Override
     public void onRoutingSuccess(PolylineOptions mPolyOptions, Route route) {
-        progressDialog.dismiss();
         CameraUpdate center = CameraUpdateFactory.newLatLng(start);
         CameraUpdate zoom = CameraUpdateFactory.zoomTo(16);
 
         map.moveCamera(center);
-
 
         if (polyline != null)
             polyline.remove();
