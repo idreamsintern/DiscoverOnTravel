fed77f44f63269c4c46d4325d69b7a2345c14866
chichunchen <chichunchen844@gmail.com>
2015-07-18 22:57:12 +0800

    Format code.

--- app/src/main/java/com/example/idreams/dot/MainActivity.java
+++ app/src/main/java/com/example/idreams/dot/MainActivity.java
@@ -7,7 +7,6 @@ import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,38 +14,31 @@ import com.example.idreams.dot.chat.ChatActivity;
 import com.example.idreams.dot.localtopics.BoardActivity;
 import com.example.idreams.dot.nearby.NearbyActivity;
 import com.example.idreams.dot.utils.GetToken;
-import com.example.idreams.dot.utils.RestClient;
-import com.loopj.android.http.JsonHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-
-import org.apache.http.Header;
-import org.json.JSONObject;
 
 
 public class MainActivity extends BaseActivity implements FragmentSimpleLoginButton.MyInterface {
 
-    TextView tv;
     public static final int INDEX_SIMPLE_LOGIN = 0;
-
-    private static final String STATE_SELECTED_FRAGMENT_INDEX = "selected_fragment_index";
     public static final String FRAGMENT_TAG = "fragment_tag";
-    private FragmentManager mFragmentManager;
-
+    private static final String STATE_SELECTED_FRAGMENT_INDEX = "selected_fragment_index";
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     public static String tokenstring = "api_doc_token";
+    TextView tv;
+    private FragmentManager mFragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.tv);
-        FragmentSimpleLoginButton fragment=new FragmentSimpleLoginButton();
+        FragmentSimpleLoginButton fragment = new FragmentSimpleLoginButton();
         mFragmentManager = getSupportFragmentManager();
         toggleFragment(INDEX_SIMPLE_LOGIN);
         GetToken.getToken();
     }
+
     public void getMessage(String msg) {
-        String mUsername="";
+        String mUsername = "";
         tv.setText(msg);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //mUsername = prefs.getString("username", "traveller");
@@ -78,9 +70,9 @@ public class MainActivity extends BaseActivity implements FragmentSimpleLoginBut
     private void toggleFragment(int index) {
         Fragment fragment = mFragmentManager.findFragmentByTag(FRAGMENT_TAG);
         FragmentTransaction transaction = mFragmentManager.beginTransaction();
-        switch (index){
+        switch (index) {
             case INDEX_SIMPLE_LOGIN:
-                transaction.replace(android.R.id.content, new FragmentSimpleLoginButton(),FRAGMENT_TAG);
+                transaction.replace(android.R.id.content, new FragmentSimpleLoginButton(), FRAGMENT_TAG);
                 break;
 
         }
