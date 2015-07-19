5f7df18561e4b2da815b4acc3e4d35f34d62b84e
chichunchen <chichunchen844@gmail.com>
2015-07-20 03:45:15 +0800

    Add SwipeRefresh for TopicListActivity, but sadly the api didn't supply paginate...

--- app/src/main/java/com/example/idreams/dot/localtopics/TopicListActivity.java
+++ app/src/main/java/com/example/idreams/dot/localtopics/TopicListActivity.java
@@ -3,6 +3,7 @@ package com.example.idreams.dot.localtopics;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.util.Log;
 import android.view.View;
@@ -31,7 +32,8 @@ public class TopicListActivity extends BaseActivity {
     ProgressDialog progressbar;
     ArrayList<Topic> topicArrayList;
 
-    ListView listView;
+    private ListView listView;
+    private SwipeRefreshLayout swipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,14 +68,17 @@ public class TopicListActivity extends BaseActivity {
         listView.setAdapter(adapter);
 
         progressbar = ProgressDialog.show(this, "下載資料", "請稍待片刻...", true);
-        getData();
+        // fetch 10 items for the first time to improve user experience.
+        getData(10);
+
+        bindSwipeRefresh();
     }
 
-    private void getData() {
+    private void getData(int limit) {
         Log.e(LOG_TAG, "getdata()");
         RequestParams params = new RequestParams();
         params.put("period", 10);
-        params.put("limit", 50);
+        params.put("limit", limit);
         params.put("board", board_string);
         params.put("token", MainActivity.tokenstring);
         RestClient.post(url, params, new JsonHttpResponseHandler() {
@@ -91,8 +96,6 @@ public class TopicListActivity extends BaseActivity {
 //                        Log.e(LOG_TAG, "onSuccess" + board + title + url + push + time);
                         adapter.add(new Topic(board, title, url, push, time));
                     }
-
-
                 } catch (Exception err) {
                     Log.e(LOG_TAG, err.getMessage());
                 }
@@ -111,5 +114,26 @@ public class TopicListActivity extends BaseActivity {
                 progressbar.dismiss();
             }
         });
+    }
+
+    private void bindSwipeRefresh() {
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Remember to CLEAR OUT old items before appending in the new ones
+                adapter.clear();
+                // ...the data has come back, add new items to your adapter...
+                getData(30);
+                // Now we call setRefreshing(false) to signal refresh has finished
+                swipeContainer.setRefreshing(false);
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
     }
 }
