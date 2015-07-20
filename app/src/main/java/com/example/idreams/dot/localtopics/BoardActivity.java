4e0d9e0f5b45dda3a698ce827c39b0f21f7759bd
chichunchen <chichunchen844@gmail.com>
2015-07-20 17:18:11 +0800

    Connect the new recycler BoardActivity to TopicListActivity.

--- app/src/main/java/com/example/idreams/dot/localtopics/BoardActivity.java
+++ app/src/main/java/com/example/idreams/dot/localtopics/BoardActivity.java
@@ -3,11 +3,14 @@ package com.example.idreams.dot.localtopics;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import com.example.idreams.dot.BaseActivity;
 import com.example.idreams.dot.R;
 
 import java.util.ArrayList;
+
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator;
 
 public class BoardActivity extends BaseActivity {
 
@@ -18,27 +21,35 @@ public class BoardActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
-//        final String[] boardStringArray = getResources().getStringArray(R.array.board);
 
         boardRecyclerView = (RecyclerView) findViewById(R.id.rv_board);
 
         BoardRecyclerViewAdapter adapter = new BoardRecyclerViewAdapter(this, getBoards());
         boardRecyclerView.setAdapter(adapter);
         boardRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         // Optimizations if all item views are of the same height and width
         // for significantly smoother scrolling.
         boardRecyclerView.setHasFixedSize(true);
 
+        // First param is number of columns and second param is orientation i.e Vertical or Horizontal
+        StaggeredGridLayoutManager gridLayoutManager =
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        // Attach the layout manager to the recycler view
+        boardRecyclerView.setLayoutManager(gridLayoutManager);
+        boardRecyclerView.setItemAnimator(new SlideInUpAnimator());
     }
 
     private ArrayList<Board> getBoards() {
-        ArrayList<Board> items = new ArrayList<>();
-        items.add(new Board("Gossip"));
-        items.add(new Board("lol"));
-        items.add(new Board("nba"));
-        items.add(new Board("mlb"));
+        String[] boardStringArray = getResources().getStringArray(R.array.board);
 
-        return items;
+        ArrayList<Board> boardArrayList = new ArrayList<>();
+
+        for (int i = 0; i < boardStringArray.length; i++) {
+            boardArrayList.add(new Board(boardStringArray[i]));
+        }
+
+        return boardArrayList;
     }
 
     private void setListenter() {
--- app/src/main/java/com/example/idreams/dot/localtopics/BoardRecyclerViewAdapter.java
+++ app/src/main/java/com/example/idreams/dot/localtopics/BoardRecyclerViewAdapter.java
@@ -1,11 +1,13 @@
 package com.example.idreams.dot.localtopics;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.idreams.dot.R;
 
@@ -22,15 +24,29 @@ public class BoardRecyclerViewAdapter extends
         this.context = context;
     }
 
-    private ArrayList<Board> boards;
+    private static ArrayList<Board> boards;
     private Context context;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public TextView tvName;
+        public Context context;
 
-        public ViewHolder(View itemView) {
+        public ViewHolder(View itemView, Context context) {
             super(itemView);
             this.tvName = (TextView) itemView.findViewById(R.id.tvName);
+            this.context = context;
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getLayoutPosition(); // gets item position
+            Board board = boards.get(position);
+
+            Intent intent = new Intent(context, TopicListActivity.class);
+            intent.putExtra("BoardName", board.getName());
+            context.startActivity(intent);
         }
     }
 
@@ -40,7 +56,7 @@ public class BoardRecyclerViewAdapter extends
         View itemView = LayoutInflater.from(context).
                 inflate(R.layout.item_board, parent, false);
 
-        return new BoardRecyclerViewAdapter.ViewHolder(itemView);
+        return new BoardRecyclerViewAdapter.ViewHolder(itemView, context);
     }
 
     @Override
